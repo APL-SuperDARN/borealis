16ea0a6ff61896035c54fe08f449254851fb7ff2
Jordan Wiker <jordan.wiker@gmail.com>
2026-03-25 11:26:36 -0400

    Restore full_fov CW default test coverage

--- tests/experiments/experiment_unittests.py
+++ tests/experiments/experiment_unittests.py
@@ -160,13 +160,28 @@ class TestExperimentRetrieval(unittest.TestCase):
     def test_module_with_extra_attributes(self):
         retrieve_experiment("full_fov")
 
-    def test_full_fov_default_lfm_metadata(self):
+    def test_full_fov_default_cw_metadata(self):
         from utils import log_config
 
         log_config.log(console=False, logfile=False, aggregator=False)
 
         exp_class = retrieve_experiment("full_fov")
         exp = exp_class()
+        slc = exp.slice_dict[0]
+
+        self.assertEqual(slc.pulse_waveform, "cw")
+        self.assertAlmostEqual(slc.pulse_waveform_bandwidth, 0.0)
+        self.assertTrue(slc.acf)
+        self.assertTrue(slc.xcf)
+        self.assertTrue(slc.acfint)
+
+    def test_full_fov_explicit_lfm_metadata(self):
+        from utils import log_config
+
+        log_config.log(console=False, logfile=False, aggregator=False)
+
+        exp_class = retrieve_experiment("full_fov")
+        exp = exp_class(pulse_waveform="lfm")
         slc = exp.slice_dict[0]
 
         self.assertEqual(slc.pulse_waveform, "lfm")
